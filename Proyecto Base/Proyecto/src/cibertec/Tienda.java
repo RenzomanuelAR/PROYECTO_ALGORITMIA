fa1e88391ab993222c2d979a36b1ba12aa691ef4
Renzo <renzolive221@gmail.com>
2024-04-12 18:46:59 -0500

    Proyecto Inicial

--- Proyecto Base/Proyecto/src/cibertec/Tienda.java
+++ Proyecto Base/Proyecto/src/cibertec/Tienda.java
@@ -14,39 +14,39 @@ public class Tienda extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	
-	// Datos mínimos del primer colchón
+	// Datos mï¿½nimos del primer colchï¿½n
 	public static String marca0 = "Suavestar";
 	public static double precio0 = 499.0;
 	public static int garantia0 = 7;
-	public static String tamaño0 = "1 1/2 Plaza";
+	public static String tamanio0 = "1 1/2 Plaza";
 	public static String material0 = "Tela Tricot Acolchada";
 	
-	// Datos mínimos del segundo colchón
+	// Datos mï¿½nimos del segundo colchï¿½n
 	public static String marca1 = "Springwall";
 	public static double precio1 = 679.0;
 	public static int garantia = 10;
-	public static String tamaño1 = "2 Plazas";
-	public static String material1 = "Tejido de Punto con Algodón Orgánico";
+	public static String tamanio1 = "2 Plazas";
+	public static String material1 = "Tejido de Punto con AlgodÃ³n OrgÃ¡nico";
 	
-	// Datos mínimos del tercer colchón
+	// Datos mï¿½nimos del tercer colchï¿½n
 	public static String marca2 = "Paraiso";
 	public static double precio2 = 479.0;
 	public static int garantia2 = 5;
-	public static String tamaño2 = "1 1/2 Plazas";
+	public static String tamanio2 = "1 1/2 Plazas";
 	public static String material2 = "Tejido de Punto";
 	
-	// Datos mínimos del cuarto colchón
+	// Datos mï¿½nimos del cuarto colchï¿½n
 	public static String marca3 = "Drimer";
 	public static double precio3 = 749.0;
 	public static int garantia3 = 4;
-	public static String tamaño3 = "Queen";
+	public static String tamanio3 = "Queen";
 	public static String material3 = "Jacquard";
 	
-	// Datos mínimos del quinto colchón
+	// Datos mï¿½nimos del quinto colchï¿½n
 	public static String marca4 = "Cisne";
 	public static double precio4 = 389.0;
 	public static int garantia4 = 2;
-	public static String tamaño4 = "1 1/2 Plazas";
+	public static String tamanio4 = "1 1/2 Plazas";
 	public static String material4 = "Tejido de Punto";
 	
 	// Porcentajes de descuento
@@ -55,16 +55,16 @@ public class Tienda extends JFrame implements ActionListener {
 	public static double porcentaje3 = 12.5;
 	public static double porcentaje4 = 15.0;
 	
-	// Cantidad óptima de colchones vendidos
+	// Cantidad ï¿½ptima de colchones vendidos
 	public static int cantidadOptima = 10;
 	
-	// Cantidad mínima de colchones adquiridos para acceder al obsequio
+	// Cantidad mï¿½nima de colchones adquiridos para acceder al obsequio
 	public static int cantidadMinimaObsequiable = 3;
 		
-	// Obsequio por cantidad mínima de colchones adquiridos
+	// Obsequio por cantidad mï¿½nima de colchones adquiridos
 	public static String obsequio = "USB";
 	
-	// Número de cliente que recibe el premio sorpresa
+	// Nï¿½mero de cliente que recibe el premio sorpresa
 	public static int numeroClienteSorpresa = 5;
 	
 	// Premio sorpresa
